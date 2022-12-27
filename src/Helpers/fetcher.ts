5a0150f7502303bf3d5625c8ab1bab400f2372d6
Nguyễn Thiên Quang <nguyenthienquang.it1994@gmail.com>
2022-12-27 15:12:10 +0700

    add loading animation when feching

--- src/Helpers/fetcher.ts
+++ src/Helpers/fetcher.ts
@@ -15,6 +15,7 @@ export const fetcher = <T = any>(option: string | { uri: string, body?: any, met
 }
 
 export const fetcherSWR  = async <T = any>(option: string | { uri: string, body?: any, method?: keyof Axios | string }, config?: AxiosRequestConfig): Promise<T> => {
+    await new Promise(resolve => setTimeout(resolve, 500))
     const {data} = await fetcher(option, config);
     return data;
 }
